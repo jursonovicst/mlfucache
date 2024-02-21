2c3c3a5656f293326b8d87c3ad4aaf68fda51941
t.jursonovics <tamas.jursonovics@telekom.de>
2024-02-21 16:30:32 +0100

    something does not sum up

--- benchmark/beladycache.py
+++ benchmark/beladycache.py
@@ -2,26 +2,18 @@ from cachetools import Cache
 
 
 class BeladyCache(Cache):
-    """Least Frequently Used (LFU) cache implementation with memory."""
+    """Bélády's cache implementation."""
 
     def __init__(self, maxsize, future, getsizeof=None):
         Cache.__init__(self, maxsize, getsizeof)
         self.__future = future
-        self.__pointer = 0
+        self.__pointer = -1
 
-    @property
-    def future(self):
-        return self.__future[self.__pointer:]
-
-#    def __setitem__(self, key, value):
-#        Cache.__setitem__(self, key, value)
-
-    def __getitem__(self, item):
-#        self.__pointer += 1
-#        print(self.__pointer)
-        Cache.__getitem__(self, item)
+    def __getitem__(self, key, cache_getitem=Cache.__getitem__):
+        self.__pointer += 1
+        return cache_getitem(self, key)
 
     def popitem(self):
         """Remove and return the `(key, value)` pair longest not needed again."""
-        key = max(self.keys(), key=self.future.index)
+        key = max(self, key=lambda k: self.__future.index(k, self.__pointer) if k in self.__future else float('inf'))
         return key, self.pop(key)
