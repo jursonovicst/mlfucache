2e1eb5dcb983538aeafc7c71d05b23b679e5ba35
jursonovicst <github.com@jursonovics.com>
2024-02-29 08:22:27 +0100

    uodate

--- benchmark/beladycache.py
+++ benchmark/beladycache.py
@@ -7,13 +7,20 @@ class BeladyCache(Cache):
     def __init__(self, maxsize, future, getsizeof=None):
         Cache.__init__(self, maxsize, getsizeof)
         self.__future = future
-        self.__pointer = -1
+        self.__time = 0
 
-    def __getitem__(self, key, cache_getitem=Cache.__getitem__):
-        self.__pointer += 1
-        return cache_getitem(self, key)
+    @property
+    def time(self) -> int:
+        return self.__time
+
+    @time.setter
+    def time(self, v: int) -> None:
+        if v < 0:
+            raise ValueError(f"None negative time: {v}")
+        self.__time = v
 
     def popitem(self):
         """Remove and return the `(key, value)` pair longest not needed again."""
-        key = max(self, key=lambda k: self.__future.index(k, self.__pointer) if k in self.__future else float('inf'))
+        key = max(self, key=lambda k: self.__future.index(k, self.__time) if k in self.__future else float('inf'))
+        #print(f"Pop {key} at {self.time}")
         return key, self.pop(key)
