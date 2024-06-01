69380e9acc5584a1fa95724ade5d2f24e0128a36
jursonovicst <github.com@jursonovics.com>
2024-06-01 15:16:26 +0200

    Fix issue at counting.

--- mlfucache.py
+++ mlfucache.py
@@ -4,51 +4,41 @@ from cachetools import Cache
 
 
 class MLFUCache(Cache):
-    """Least Frequently Used (LFU) cache implementation with memory."""
-
-    #     def __init__(self, maxsize, getsizeof=None):
-    #         Cache.__init__(self, maxsize, getsizeof)
-    #         self.__counter = collections.Counter()
+    """Least Frequently Used (LFU) cache implementation."""
 
     def __init__(self, maxsize, getsizeof=None):
         Cache.__init__(self, maxsize, getsizeof)
         self.__counter = collections.Counter()
-        self.__parked = collections.Counter()
-
-    #     def __getitem__(self, key, cache_getitem=Cache.__getitem__):
-    #         value = cache_getitem(self, key)
-    #         if key in self:  # __missing__ may not store item
-    #             self.__counter[key] -= 1
-    #         return value
+        self.__parked = collections.Counter()  # <-- this is new
 
     def __getitem__(self, key, cache_getitem=Cache.__getitem__):
         value = cache_getitem(self, key)
         if key in self:  # __missing__ may not store item
-            self.__counter[key] -= 1 if key not in self.__parked else self.__parked.pop(key) + 1
+            self.__counter[key] -= 1
         return value
-
-    #     def __setitem__(self, key, value, cache_setitem=Cache.__setitem__):
-    #         cache_setitem(self, key, value)
-    #         self.__counter[key] -= 1
 
     def __setitem__(self, key, value, cache_setitem=Cache.__setitem__):
         cache_setitem(self, key, value)
-        self.__counter[key] -= 1 if key not in self.__parked else self.__parked.pop(key) + 1
-
-    #     def __delitem__(self, key, cache_delitem=Cache.__delitem__):
-    #         cache_delitem(self, key)
-    #         del self.__counter[key]
+        self.__counter[key] -= 1 if key not in self.__parked else self.__parked.pop(key) + 1  # <-- this is new
 
     def __delitem__(self, key, cache_delitem=Cache.__delitem__):
         cache_delitem(self, key)
-        self.__parked[key] = -self.__counter.pop(key)  # <-- this is new
+        self.__parked[key] = - self.__counter.pop(key)
 
-    # no change
     def popitem(self):
         """Remove and return the `(key, value)` pair least frequently used."""
         try:
             ((key, _),) = self.__counter.most_common(1)
+            self.__counter[key] += 1  # <-- this will generate an extra getitem, compensate
         except ValueError:
             raise KeyError("%s is empty" % type(self).__name__) from None
         else:
             return (key, self.pop(key))
+
+    @property
+    def _debug_c(self):
+        return self.__counter
+
+    @property
+    def _debug_p(self):
+        return self.__parked
--- test_mlfucache.py
+++ test_mlfucache.py
@@ -6,11 +6,44 @@ from mlfucache import MLFUCache
 class TestMLFUCache(TestCase):
     def test_mlfucache(self):
         c = MLFUCache(2)
-        self.assertEquals(2, c.maxsize)
+        self.assertEqual(2, c.maxsize)
 
-        c[1] = 1
-        c[2] = 2
-        self.assertEquals(1, c[1])
-        self.assertEquals(1, c.pop(1))
+        self.assertDictEqual(c._debug_c, {})
+        self.assertDictEqual(c._debug_p, {})
+
+        c[1] = None
+        self.assertDictEqual(c._debug_c, {1: -1})
+        self.assertDictEqual(c._debug_p, {})
+
+        c[1] = None
+        self.assertDictEqual(c._debug_c, {1: -2})
+        self.assertDictEqual(c._debug_p, {})
+
+        self.assertIsNone(c[1])
+        self.assertDictEqual(c._debug_c, {1: -3})
+        self.assertDictEqual(c._debug_p, {})
+
+        c[2] = None
+        self.assertDictEqual(c._debug_c, {1: -3, 2: -1})
+        self.assertDictEqual(c._debug_p, {})
+
+        c[2] = None
+        self.assertDictEqual(c._debug_c, {1: -3, 2: -2})
+        self.assertDictEqual(c._debug_p, {})
+
+        c[3] = None
+        self.assertDictEqual(c._debug_c, {1: -3, 3: -1})
+        self.assertDictEqual(c._debug_p, {2: 2})
+
+        c[2] = None
+        self.assertDictEqual(c._debug_c, {1: -3, 2: -3})
+        self.assertDictEqual(c._debug_p, {3: 1})
+
         with self.assertRaises(KeyError):
-            c[1]
+            c[4]
+        self.assertDictEqual(c._debug_c, {1: -3, 2: -3})
+        self.assertDictEqual(c._debug_p, {3: 1})
+
+        self.assertEqual(None, c.pop(2))
+        self.assertDictEqual(c._debug_c, {1: -3})
+        self.assertDictEqual(c._debug_p, {3: 1, 2: 4})  # this is wrong!
